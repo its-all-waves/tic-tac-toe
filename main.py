8608128242a0214994355639848eedc5bf9c2fe1
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2025-10-11 13:19:14 -0500

    add: take user input and validate; add: rules printout and confirmation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-type PlayerSign = Literal["x", "o"]
+type PlayerSign = Literal["X", "O"]
 
 type PlayerEmoji = Literal["❌", "⭕"]
 
@@ -20,14 +20,21 @@ class Player:
         return self.emoji
 
 
-class Board:
-    empty = "⬜"
+type EmptyEmoji = Literal["⬜"]
 
-    b: list[list[str]] = [
+
+class Board:
+    empty: EmptyEmoji = "⬜"
+
+    b: list[list[EmptyEmoji | PlayerEmoji]] = [
         [empty, empty, empty],
         [empty, empty, empty],
         [empty, empty, empty],
     ]
+
+    def add_move(self, turn: PlayerSign, coords: tuple[int, int]):
+        col, row = coords
+        self.b[row][col] = sign_map[turn]
 
     def print(self):
         b = self.b
@@ -39,9 +46,64 @@ class Board:
         print()
 
 
+player_turn: PlayerSign = "X"
+
+rules = """
+[ Rules ]
+
+⇢ X goes first.
+
+⇢ Enter coordinates as [X, Y] or [column, row]. For example, to make your move at column 3, row 2, enter 'C2'.
+
+⇢ Case does not matter.
+"""
+
+
+def isInputValid(inp: str) -> bool:
+    inp = inp.strip()
+    if len(inp) != 2:
+        return False
+    if inp[0] not in ("A", "B", "C", "a", "b", "c"):
+        return False
+    if inp[1] not in ("1", "2", "3"):
+        return False
+    return True
+
+
+# fmt: off
+# map user-entered coord to board index
+move_map: dict[str, int] = {
+    "A": 0, "B": 1, "C": 2,
+    "a": 0, "b": 1, "c": 2,
+    "1": 0, "2": 1, "3": 2,
+}
+# fmt: on
+
+sign_map: dict[PlayerSign, PlayerEmoji] = {
+    "X": "❌",
+    "O": "⭕",
+}
+
 
 def main():
+    # wait for user to confirm understanding of rules
+    print(rules)
+    while True:
+        if input("Capisce? Press enter to play!") == "":
+            break
+
     board = Board()
+    board.print()
+
+    # wait for valid input
+    move: str
+    while True:
+        move = input(f"Make a move (e.g. B3).\n{player_turn} at: ")
+        if isInputValid(move):
+            break
+
+    col, row = move
+    board.add_move(player_turn, coords=(move_map[col], move_map[row]))
     board.print()
 
 
